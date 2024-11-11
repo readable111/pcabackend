4734453aa4eceaa75bccb0f8cd9210f8b9c7ee31
readable111 <bowent111@gmail.com>
2024-11-11 16:37:20 -0600

    figured it out

--- server.py
+++ server.py
@@ -141,10 +141,10 @@ def getCropsverbose(subID):
     try:
         cur = conn.cursor()
         query = """SELECT DISTINCT c.*, m.fld_m_MediumType, l.fld_l_LocatonName, f.fld_f_FarmName, ct.fld_ct_CropTypeName FROM tbl_crops AS c 
-        INNER JOIN tbl_media AS m ON c.fld_m_MediumID_fk = m.fld_m_MediumID_pk
-        INNER JOIN tbl_locations AS l ON  c.fld_l_LocationID_fk = l.fld_l_LocationID_pk
-        INNER JOIN tbl_farms AS f ON   c.fld_f_FarmID_fk = f.fld_f_FarmID_pk
-        INNER JOIN tbl_croptypes as ct ON c.fld_ct_CropTypeID_fk = ct.fld_ct_CropTypeID_pk
+        JOIN tbl_media AS m ON c.fld_m_MediumID_fk = m.fld_m_MediumID_pk
+        JOIN tbl_locations AS l ON  c.fld_l_LocationID_fk = l.fld_l_LocationID_pk
+        JOIN tbl_farms AS f ON   c.fld_f_FarmID_fk = f.fld_f_FarmID_pk
+        JOIN tbl_croptypes as ct ON c.fld_ct_CropTypeID_fk = ct.fld_ct_CropTypeID_pk
         WHERE c.fld_s_SubscriberID_pk = %s;
                 """
         cur.execute(query,(subID,))
