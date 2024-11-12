b9288333d5ed01cd8c309c52071bdba86017aa3c
tjb0264 <tylerbowen@my.unt.edu>
2024-11-12 12:53:52 -0600

    fixed the syntax error

--- server.py
+++ server.py
@@ -284,15 +284,15 @@ def connect():
 def listTasks(subID):
     try:
         cur = conn.cursor()
-        query = """"SELECT t.*,  tt.fld_tt_TaskTypeName FROM tbl_tasks AS t
-        INNER JOIN tbl_tasktypes AS tt ON t.fld_s_SubscriberID_pk = tt.fld_s_SubscriberID_pk AND t.fld_t_TaskTypeID_fk = tt.fld_tt_TaskTypeID_pk
+        query = """SELECT t.*,  tt.fld_tt_TaskTypeName FROM tbl_tasks AS t
+        JOIN tbl_taskTypes AS tt ON t.fld_s_SubscriberID_pk = tt.fld_s_SubscriberID_pk AND t.fld_t_TaskTypeID_fk = tt.fld_tt_TaskTypeID_pk
         WHERE fld_s_SubscriberID_pk = %s;"""
         cur.execute(query, (subID,))
         results = cur.fetchall()
         return jsonify(results), 200
     except Exception as e:
         print(f"Error: {e}")
-        return "Error connecting to database"
+        return "Error connecting to database", 500
     finally:
         cur.close()
 
